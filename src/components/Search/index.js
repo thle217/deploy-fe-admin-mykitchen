da0e9ba5e89109eb3f286228f86c0f6717ae7a10
thle217 <lethihau.d19@gmail.com>
2023-03-13 17:38:24 +0700

    update crud brand

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom";
 function Search() {
+     //CALL API
+     
+
+     const navigate = useNavigate();
+     const handleSubmit = event => {
+       event.preventDefault();
+       navigate('/');
+       
+       const keyword = document.getElementById("search-box").value
+
+       
+     };
+
+
     return (
-        <form action="" method="post" className="d-none d-md-flex ms-4">
+        <form action="#" method="get" className="d-none d-md-flex ms-4" onSubmit={handleSubmit}>
             <input
+            id="search-box"
                 className="form-control bg-secondary border-1"
                 type="search"
                 placeholder="Tìm kiếm..."
@@ -9,7 +25,7 @@ function Search() {
             />
             <button className="btn-search btn-light border-0 text-dark" name="btn-search">TÌM</button>
         </form>
-    )
+        )
 }
 
 export default Search;
